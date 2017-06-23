fecd80998c5ff6d6c651aa43773a1ee99bcc4a63
Entropy224 <michael@stadlmeier.de>
2017-06-23 22:39:06 +0200

    Added mzxml parser

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,9 @@
+import uk.ac.ebi.pride.tools.jmzreader.JMzReaderException;
+import uk.ac.ebi.pride.tools.jmzreader.model.Spectrum;
+import uk.ac.ebi.pride.tools.mzxml_parser.MzXMLFile;
+import uk.ac.ebi.pride.tools.mzxml_parser.MzXMLParsingException;
+import uk.ac.ebi.pride.tools.mzxml_parser.MzXMLSpectrum;
+
 import java.io.File;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -7,10 +13,10 @@ import java.util.ArrayList;
  */
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws MzXMLParsingException, JMzReaderException {
         DecimalFormat fiveDec = new DecimalFormat("0.00000");
 
-        String filePath = "C:\\Anwendungen\\IntelliJProjects\\NiceSpectraAnalyzer\\NiceSpectraAnalyzer\\Aminoacids_list.csv";
+        /*String filePath = "C:\\Anwendungen\\IntelliJProjects\\NiceSpectraAnalyzer\\NiceSpectraAnalyzer\\Aminoacids_list.csv";
         File aminoAcids = new File(filePath);
         ArrayList<AminoAcid> aminoAcidsList = new ArrayList<>();
         aminoAcidsList = CSVReader.aminoAcidParse(aminoAcids);
@@ -32,10 +38,10 @@ public class Main {
             FragmentIon.fragmentIonPrinter(fragment);
         }
        ArrayList<IonMatch> matchedIons = PeakCompare.peakCompare(testSpectrum, pepAMod, 5);
+*/
 
-
-
-
+        MzXMLFile file = new MzXMLFile(new File("C:\\Users\\Michael Stadlmeier\\Desktop\\Programmierzeugs\\20170519_stamch_ECDuplex_NEBBSA_newbatches_1to1_R1.mzxml"));
+        Spectrum spectrum = file.getSpectrumById("2050");
 
 
 
